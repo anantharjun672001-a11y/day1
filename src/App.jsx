610ddbd31971d79e196eebbc9483444c30b93d35
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-02 19:23:20 +0530

    components call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div>
-            <h1>App Component</h1>
+            <Header />
+            <Home />
+            <Footer />
         </div>
     );
 };
